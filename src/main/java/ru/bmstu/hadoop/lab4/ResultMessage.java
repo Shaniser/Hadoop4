1485d8c7657a6350b13aed842e7999000d7352b9
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-12-23 00:05:14 +0300

    Scripted auto-commit on change (2020-12-23 00:05:14) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab4/ResultMessage.java
+++ src/main/java/ru/bmstu/hadoop/lab4/ResultMessage.java
@@ -1,10 +1,14 @@
 package ru.bmstu.hadoop.lab4;
 
+import java.util.ArrayList;
+
 public class ResultMessage {
     private final String packageId;
+    private final ArrayList<String> result;
 
-    public ResultMessage(String packageId) {
+    public ResultMessage(String packageId, ArrayList<String> result) {
         this.packageId = packageId;
+        this.result = result;
     }
 
     public String getPackageId() {
--- src/main/java/ru/bmstu/hadoop/lab4/StoreActor.java
+++ src/main/java/ru/bmstu/hadoop/lab4/StoreActor.java
@@ -1,4 +1,23 @@
 package ru.bmstu.hadoop.lab4;
 
-public class StoreActor {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class StoreActor extends AbstractActor {
+    private final HashMap<String, ArrayList<String>> testResults = new HashMap<>();
+
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(ResultMessage.class,
+                        msg -> {
+                            String id = msg.getPackageId();
+                            String result = testResults.get(id);
+                            sender().tell(new ResultMessage(id, result), sender());
+                        })
+    }
 }
