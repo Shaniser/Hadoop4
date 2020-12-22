b2938c1082bfe6928b314b042fd352bd208e5112
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-12-23 00:07:59 +0300

    Scripted auto-commit on change (2020-12-23 00:07:59) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab4/StoreActor.java
+++ src/main/java/ru/bmstu/hadoop/lab4/StoreActor.java
@@ -12,12 +12,14 @@ public class StoreActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
+        ArrayList<String> results = new ArrayList<>();
+        results.add("test");
         return ReceiveBuilder.create()
-                .match(ResultMessage.class,
-                        msg -> {
-                            String id = msg.getPackageId();
-                            String result = testResults.get(id);
-                            sender().tell(new ResultMessage(id, result), sender());
-                        })
+                .match(ResultMessage.class, msg -> {
+                    testResults.put("2", results);
+                    String id = msg.getPackageId();
+                    ArrayList<String> result = testResults.get(id);
+                    sender().tell(new ResultMessage(id, result), sender());
+                }).build();
     }
 }
