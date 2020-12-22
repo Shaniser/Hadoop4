0aefcc076f7c7efdb398744518320d5e2641d6fb
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-12-22 22:57:05 +0300

    Scripted auto-commit on change (2020-12-22 22:57:05) by gitwatch.sh

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -1,5 +1,11 @@
+import javax.script.ScriptEngine;
+
 public class Tester {
     public static void main(String[] args) {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(jscript);
+        Invocable invocable = (Invocable) engine;
+        return invocable.invokeFunction(functionName, params).toString();
 
     }
 }
