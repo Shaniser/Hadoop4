b9385fa34d570a2ba11bdba86574e0cbcf5db257
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-12-22 23:01:47 +0300

    Scripted auto-commit on change (2020-12-22 23:01:47) by gitwatch.sh

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -1,13 +1,14 @@
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class Tester {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ScriptException, NoSuchMethodException {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
         engine.eval("var f = function(a, b) {return a + b}");
         Invocable invocable = (Invocable) engine;
-        System.out.println(invocable.invokeFunction("f", params).toString());
+        System.out.println(invocable.invokeFunction("f", 5, 12).toString());
 
     }
 }
