e5e9450df9b5169b125e8157c30e770579698941
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-12-22 23:13:17 +0300

    Scripted auto-commit on change (2020-12-22 23:13:17) by gitwatch.sh

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -1,3 +1,6 @@
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -5,10 +8,25 @@ import javax.script.ScriptException;
 
 public class Tester {
     public static void main(String[] args) throws ScriptException, NoSuchMethodException {
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-        engine.eval("var f = function(a, b) {return a + b}");
-        Invocable invocable = (Invocable) engine;
-        System.out.println(invocable.invokeFunction("f", 5, 12).toString());
+//        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+//        engine.eval("var f = function(a, b) {return a + b}");
+//        Invocable invocable = (Invocable) engine;
+//        System.out.println(invocable.invokeFunction("f", 5, 12).toString());
 
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        MainHttp instance = new MainHttp(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+                System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 }
