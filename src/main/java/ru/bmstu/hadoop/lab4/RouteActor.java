c37d8da66dc3e72293904e7d1759486d78b5c64c
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-12-22 23:51:31 +0300

    Scripted auto-commit on change (2020-12-22 23:51:31) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab4/RouteActor.java
+++ src/main/java/ru/bmstu/hadoop/lab4/RouteActor.java
@@ -4,16 +4,17 @@ package ru.bmstu.hadoop.lab4;
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
+import akka.japi.pf.ReceiveBuilder;
 import akka.routing.RoundRobinPool;
 
 
 public class RouteActor extends AbstractActor {
     private final ActorRef actorExecutor = getContext().actorOf(
             new RoundRobinPool(5).props(Props.create(ExecuteActor.class)));
-    private final ActorRef 
+    private final ActorRef actorStorage = getContext().actorOf((Props.create(StoreActor.class)));
 
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create().match();
     }
 }
