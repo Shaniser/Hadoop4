a96674ede1b09627dc2eaa01b9f2ec7728e65541
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-12-22 23:33:14 +0300

    Scripted auto-commit on change (2020-12-22 23:33:14) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab4/Request.java
+++ src/main/java/ru/bmstu/hadoop/lab4/Request.java
@@ -1,4 +1,21 @@
 package ru.bmstu.hadoop.lab4;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Request {
+    @JsonProperty("packageId")
+    private final String packageId;
+    @JsonProperty("jsScript")
+    private final String jsScript;
+    @JsonProperty("functionName")
+    private final String functionName;
+    @JsonProperty("tests")
+    private final String tests;
+
+    public Request(String packageId, String jsScript, String functionName, String tests) {
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.tests = tests;
+    }
 }
